bef09c4c7b0af9d49a35dcf3a8aa2e4f320475ba
Duvan Salas <duvanalexanderc11@gmail.com>
2025-03-14 15:29:15 -0500

    feat: Add superuser, Product model, and admin configuration

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User 
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con usuario vendedor
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
