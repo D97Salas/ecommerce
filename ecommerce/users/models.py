11c8992d38a5e6d4b138af76851270f36bd74393
Duvan Salas <duvanalexanderc11@gmail.com>
2025-03-14 13:48:19 -0500

    COnfiguaracion inicial, modelo users

--- ecommerce/users/models.py
+++ ecommerce/users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    is_seller = models.BooleanField(default=False)  # Si el usuario es vendedor o no
+
+    def __str__(self):
+        return self.username
