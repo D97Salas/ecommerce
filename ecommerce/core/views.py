42f7ffbd374c2c3d434d901cbd81b2ce13cd5e95
Duvan Salas <duvanalexanderc11@gmail.com>
2025-03-20 13:44:29 -0500

    feat(api): Add API views for Users, Products, and Orders

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .models import Producto, Pedido, Carrito, Articulo, Comentario
+from .serializers import (
+    ProductoSerializer,
+    PedidoSerializer,
+    CarritoSerializer,
+    ArticuloSerializer,
+    ComentarioSerializer,
+)
 
-# Create your views here.
+
+# Vistas para los modelos
+class PoductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+    permission_classes = [permissions.IsAuthenticated] # Solo usuarios logueados pueden ver los productos
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+    permission_classes = [permissions.IsAuthenticated] # Solo usuarios logueados pueden ver los pedidos
+
+class CarritoViewSet(viewsets.ModelViewSet):
+    queryset = Carrito.objects.all()
+    serializer_class = CarritoSerializer
+    permission_classes = [permissions.IsAuthenticated] # Solo usuarios logueados pueden ver los carritos
+
+class ArticuloViewSet(viewsets.ModelViewSet):
+    queryset = Articulo.objects.all()
+    serializer_class = ArticuloSerializer
+    permission_classes = [permissions.IsAuthenticated] # Solo usuarios logueados pueden ver los articulos
+
+class ComentarioViewSet(viewsets.ModelViewSet):
+    queryset = Comentario.objects.all()
+    serializer_class = ComentarioSerializer 
+    pagination_class = [permissions.IsAuthenticated] # Solo usuarios logueados pueden ver los comentarios
+
+
