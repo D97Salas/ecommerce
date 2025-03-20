1b5251dfff913632d684c9971e8b7233d1dba043
Duvan Salas <duvanalexanderc11@gmail.com>
2025-03-20 13:50:02 -0500

    feat(api): Add URLs for endpoints

---
+++ ecommerce/core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import ProductoViewSet, PedidoViewSet, CarritoViewSet, ArticuloViewSet, ComentarioViewSet
+
+router = DefaultRouter()
+router.register(r"productos", ProductoViewSet)
+router.register(r"pedidos", PedidoViewSet)
+router.register(r"carritos", CarritoViewSet)
+router.register(r"articulos", ArticuloViewSet)
+router.register(r"comentarios", ComentarioViewSet)
+
+urlpatterns = [
+    path("home/", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),  # Core app
     path("accounts/", include("allauth.urls")),  # Django Allauth
 ]
